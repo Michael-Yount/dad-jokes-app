002c9e4c578079ff5ce94c75d4dbe9059a182c8f
Michaeal Yount <cheshirekatsmile101@gmail.com>
2023-09-22 10:34:26 -0400

    app

--- dadjokes/src/App.js
+++ dadjokes/src/App.js
@@ -12,12 +12,16 @@ function App() {
     const response = await axios.get("https://api.chucknorris.io/jokes/random", {headers: {Accept:"application/json"}})
     console.log(response.data.value);
     setJoke(response.data.value);
+
+
   };
 
   const fetchDadJoke = async () => {
     const response = await axios.get("https://icanhazdadjoke.com/", {headers: {Accept:"application/json"}})
     console.log(response.data.joke);
     setDadJoke(response.data.joke);
+
+  
   };
 
 
@@ -32,7 +36,8 @@ function App() {
 
   return (
     <>
-    
+  <div>
+    <hi className="topTitle">Chuck Norris and Dad Jokes</hi>
     <div className="container">
         <h2 className='title'>Chuck Norris jokes.</h2>
         <div className="joke">{joke}</div>
@@ -44,6 +49,11 @@ function App() {
 <div className="joke">{dadJoke}</div>
 <button className="btn btn-primary" onClick={fetchDadJoke}>Get Another Dad Joke</button>
 </div>
+    </div>
+    <footer className="footer">
+      <h1>this site was made by myount</h1>
+      <image src='../public/MYlogo.png' alt="..."></image>
+    </footer>
     </>
   )
 }
