ae2667c155337571ab38f3ec5cb5758174ec252d
Michaeal Yount <cheshirekatsmile101@gmail.com>
2023-09-22 10:29:15 -0400

    app

--- dadjokes/src/App.js
+++ dadjokes/src/App.js
@@ -6,29 +6,45 @@ import './App.css';
 function App() {
   //data storage
   const [joke, setJoke] = useState([]);
+  const [dadJoke, setDadJoke] = useState([]);
+    
+  const fetchChuckJoke = async () => {
+    const response = await axios.get("https://api.chucknorris.io/jokes/random", {headers: {Accept:"application/json"}})
+    console.log(response.data.value);
+    setJoke(response.data.value);
+  };
+
+  const fetchDadJoke = async () => {
+    const response = await axios.get("https://icanhazdadjoke.com/", {headers: {Accept:"application/json"}})
+    console.log(response.data.joke);
+    setDadJoke(response.data.joke);
+  };
+
+
 
   useEffect(() => {
-    
-  const loadData = async () => {
-    const response = await axios.get("https://api.chucknorris.io/jokes/random", {headers: {Accept:"application/json"}})
-    console.log(response.data.joke);
-    setJoke(response.data.joke);
-  }
-  loadData();
-}, []);
+    fetchChuckJoke();
+  }, []);
 
-const handleClick = (event) => {
-  console.log('clicked');
+  
 
 
-};
 
   return (
+    <>
+    
     <div className="container">
-        <h2 className='title'>The Absolute Best/Worst Dad jokes.</h2>
+        <h2 className='title'>Chuck Norris jokes.</h2>
         <div className="joke">{joke}</div>
-        <button className="btn btn-primary" onClick={handleClick}>Get Another Joke</button>
+        <button className="btn btn-primary" onClick={fetchChuckJoke}>Get Another Chuck Joke</button>
       </div>
+
+<div className="container">
+<h2 className='title'>The Absolute Best Dad jokes.</h2>
+<div className="joke">{dadJoke}</div>
+<button className="btn btn-primary" onClick={fetchDadJoke}>Get Another Dad Joke</button>
+</div>
+    </>
   )
 }
 
