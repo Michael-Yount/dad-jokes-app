e70db54d3eb83f7918c26bce66b16cb99be423e4
Michaeal Yount <cheshirekatsmile101@gmail.com>
2023-09-24 19:51:09 -0400

    fixed starts with one joke each

--- dadjokes/src/App.js
+++ dadjokes/src/App.js
@@ -7,53 +7,43 @@ function App() {
   //data storage
   const [joke, setJoke] = useState([]);
   const [dadJoke, setDadJoke] = useState([]);
+  
     
   const fetchChuckJoke = async () => {
     const response = await axios.get("https://api.chucknorris.io/jokes/random", {headers: {Accept:"application/json"}})
     console.log(response.data.value);
     setJoke(response.data.value);
-
-
   };
-
   const fetchDadJoke = async () => {
     const response = await axios.get("https://icanhazdadjoke.com/", {headers: {Accept:"application/json"}})
     console.log(response.data.joke);
     setDadJoke(response.data.joke);
-
-  
   };
-
-
-
   useEffect(() => {
     fetchChuckJoke();
+    fetchDadJoke();
   }, []);
 
-  
+        return (
+          <>
+        <div>
+            <hi className="topTitle">Chuck Norris and Dad Jokes</hi>
+          <div className="container">
+              <h2 className='title'>Chuck Norris jokes.</h2>
+              <div className="joke">{joke}</div>
+              <button className="btn btn-primary" onClick={fetchChuckJoke}>Get Another Chuck Joke</button>
+              </div>
 
-
-
-  return (
-    <>
-  <div>
-    <hi className="topTitle">Chuck Norris and Dad Jokes</hi>
-    <div className="container">
-        <h2 className='title'>Chuck Norris jokes.</h2>
-        <div className="joke">{joke}</div>
-        <button className="btn btn-primary" onClick={fetchChuckJoke}>Get Another Chuck Joke</button>
-      </div>
-
-<div className="container">
-<h2 className='title'>The Absolute Best Dad jokes.</h2>
-<div className="joke">{dadJoke}</div>
-<button className="btn btn-primary" onClick={fetchDadJoke}>Get Another Dad Joke</button>
-</div>
-    </div>
-    <footer className="footer">
-      <h1>this site was made by myount</h1>
-      <image src='../public/MYlogo.png' alt="..."></image>
-    </footer>
+          <div className="container">
+              <h2 className='title'>The Absolute Best Dad jokes.</h2>
+            <div className="joke">{dadJoke}</div>
+                  <button className="btn btn-primary" onClick={fetchDadJoke}>Get Another Dad Joke</button>
+            </div>
+          </div>
+          <footer className="footer">
+            <h1>this site was made by myount</h1>
+            <image src='../public/MYlogo.png' alt="..."></image>
+          </footer>
     </>
   )
 }
